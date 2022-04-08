b9dd2dc9dfa430f2c64c425c48761d5b46b1debb
juliano988 <julio-pinheiroo@hotmail.com>
2022-04-08 11:40:21 -0300

    Alteração de teste 3-dev-1

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js 3-dev!</a>
+          Welcome to <a href="https://nextjs.org">Next.js 3-dev-1!</a>
         </h1>
 
         <p className={styles.description}>
